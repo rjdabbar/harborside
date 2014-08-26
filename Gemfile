00e15f3e6d3361042870ec66b1655389624b50ea
RJ Dabbar <rjdabbar@gmail.com>
2014-08-26 12:23:33 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,33 @@
+source 'https://rubygems.org'
+
+
+gem 'rails', '4.0.2'
+
+
+gem 'uglifier', '>= 1.3.0'
+
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem 'simple_form'
+
+
+gem 'bootstrap-sass', '~> 3.0.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'sass-rails', '~> 4.0.0'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+group :development do
+  gem "better_errors"
+  gem "binding_of_caller"
+  gem 'guard-livereload'
+  gem 'sqlite3'
+end
+
+group :production do
+  gem 'rails_12factor'
+  gem 'pg'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Harborside::Application.config.secret_key_base = '3797b049571295eb95bddb41f77fad47ed68837970307b9e636a55d60d5891aedccf2fdc372c45cbac5555fec194a2f846009ea7575bb10cb2631b393c4cff4b'
